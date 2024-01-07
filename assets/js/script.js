5c68c097b6ada20af9aec153e550b7ac14b30352
Alex Hebbes <aeh7@live.co.uk>
2024-01-07 14:58:53 +0000

    feat: modal close span added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var contactBtn = document.querySelector ('.contact-btn');
 var modalCon = document.querySelector ('.modal-con')
+var modalClose = document.querySelector ('.modal-close')
 
 contactBtn.addEventListener ('click', function(event) {
     event.preventDefault();
     modalCon.classList.add ('con-active');
 
+});
+
+modalClose.addEventListener('click', function(){
+    modalCon.classList.remove('con-active');
 });
