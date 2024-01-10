65b356b602e015af3fea1e7c2895a70cd4cdbda7
Alex Hebbes <aeh7@live.co.uk>
2024-01-10 21:09:37 +0000

    feat: began game javescript

---
+++ assets/js/game.js
@@ -0,0 +1,33 @@
+let q1 = "Q1"
+let q1_ans_a = "Q1A"
+let q1_ans_b = "Q1B"
+let q1_ans_c = "Q1C"
+
+
+document.getElementById("question").innerHTML=q1
+document.getElementById("ansBox1").innerHTML=q1_ans_a
+document.getElementById("ansBox2").innerHTML=q1_ans_b
+document.getElementById("ansBox3").innerHTML=q1_ans_c
+
+let q2 = "Q2"
+let q2_ans_a = "Q2A"
+let q2_ans_b = "Q2B"
+let q2_ans_c = "Q2C"
+
+
+document.getElementById("question").innerHTML=q2
+document.getElementById("ansBox1").innerHTML=q2_ans_a
+document.getElementById("ansBox2").innerHTML=q2_ans_b
+document.getElementById("ansBox3").innerHTML=q2_ans_c
+
+let q3 = "Q3"
+let q3_ans_a = "Q3A"
+let q3_ans_b = "Q3B"
+let q3_ans_c = "Q3C"
+
+
+document.getElementById("question").innerHTML=q3
+document.getElementById("ansBox1").innerHTML=q3_ans_a
+document.getElementById("ansBox2").innerHTML=q3_ans_b
+document.getElementById("ansBox3").innerHTML=q3_ans_c
+
