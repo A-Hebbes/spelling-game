6f92578629d70cc05bb2b36b3c55697aca1ba086
Alex Hebbes <aeh7@live.co.uk>
2024-01-17 13:29:17 +0000

    feat: tidied up script js. Also condensed styles for modals into one class. Added different class to rules modal close so that the button worked and made the styles in CSS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var contactBtn = document.querySelector ('.contact-btn');
-var modalCon = document.querySelector ('.modal-con')
+var modalCon = document.getElementById ('modal-contact')
 var modalClose = document.querySelector ('.modal-close')
+var rulesBtn = document.querySelector ('.rules-btn');
+var modalRules = document.getElementById ('modal-rules')
+var modalCloseRules = document.querySelector ('.modal-close-rules')
 
 contactBtn.addEventListener ('click', function(event) {
     event.preventDefault();
@@ -8,16 +11,7 @@ contactBtn.addEventListener ('click', function(event) {
 
 });
 
-modalClose.addEventListener('click', function(){
-    modalCon.classList.remove('con-active');
-});
-
- /* Modal for rules page need to look at why it isnt working. Changing the modalRules var stopped contact form coming up but now nothing appears and ther
- is a error in dev tools. Check this next. */
-
- var rulesBtn = document.querySelector ('.rules-btn');
- var modalRules = document.querySelector ('.modal-rules')
-
+ 
  rulesBtn.addEventListener ('click', function(event) {
     event.preventDefault();
     modalRules.classList.add ('con-active'); 
@@ -25,5 +19,13 @@ modalClose.addEventListener('click', function(){
  });
 
  modalClose.addEventListener('click', function(){
-    modalRules.classList.remove('con-active');
+    modalCon.classList.remove('con-active');
+    
 });
+
+modalCloseRules.addEventListener('click', function(){
+modalRules.classList.remove('con-active');
+    console.log ("hello")
+});
+
+ 
