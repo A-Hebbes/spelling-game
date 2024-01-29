3d17587b3e0ede3e9afea1cc773a7a0212bc2c7e
Alex Hebbes <aeh7@live.co.uk>
2024-01-29 16:00:04 +0000

    feat: tidy code format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,28 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-const contactBtn = document.querySelector ('.contact-btn');
-const modalCon = document.getElementById ('modal-contact')
-const modalClose = document.querySelector ('.modal-close')
-const rulesBtn = document.querySelector ('.rules-btn');
-const modalRules = document.getElementById ('modal-rules')
-const modalCloseRules = document.querySelector ('.modal-close-rules')
+    const contactBtn = document.getElementById('contact-btn');
+    const modalCon = document.getElementById('modal-contact')
+    const modalClose = document.querySelector('.modal-close')
+    const rulesBtn = document.getElementById('rules-btn');
+    const modalRules = document.getElementById('modal-rules')
+    const modalCloseRules = document.querySelector('.modal-close-rules')
 
-contactBtn.addEventListener ('click', function(event) {
-    event.preventDefault();
-    modalCon.classList.add ('con-active');
+    contactBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        modalCon.classList.add('con-active');
+    });
 
-});
+    rulesBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        modalRules.classList.add('con-active'); 
+    });
 
- 
- rulesBtn.addEventListener ('click', function(event) {
-    event.preventDefault();
-    modalRules.classList.add ('con-active'); 
-    
- });
+    modalClose.addEventListener('click', function(){
+        modalCon.classList.remove('con-active');
+    });
 
- modalClose.addEventListener('click', function(){
-    modalCon.classList.remove('con-active');
-    
-});
-
-modalCloseRules.addEventListener('click', function(){
-modalRules.classList.remove('con-active');
-    
-});
+    modalCloseRules.addEventListener('click', function(){
+        modalRules.classList.remove('con-active'); 
+    });
 
 });
